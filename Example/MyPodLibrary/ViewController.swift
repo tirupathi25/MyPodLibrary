2895ac7910d13b4be0713d1265d67c66366725ec
tirupathi25 <mandali.tirupathi@gmail.com>
2021-10-07 13:53:01 +0530

    my first commit

--- Example/MyPodLibrary/ViewController.swift
+++ Example/MyPodLibrary/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
-
+import MyPodLibrary
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let m = Logger()
+        m.myLogger()
     }
 
     override func didReceiveMemoryWarning() {
---
+++ MyPodLibrary/Classes/Logger.swift
@@ -0,0 +1,18 @@
+//
+//  Logger.swift
+//  MyPodLibrary
+//
+//  Created by Tirupathi Mandali on 10/7/21.
+//
+
+import Foundation
+
+public class Logger {
+    public init() {
+        
+    }
+    
+   public func myLogger()  {
+        print("logg data")
+    }
+}
